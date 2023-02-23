5562f4d2a448e413c758bcc3c93a79445c965e28
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-02-23 17:42:07 +0000

    chore: tying octokit

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
 
-try {
-    console.log(`Hello Punk!`);
-    const message = "Time: " + (new Date()).toTimeString();
-    core.setOutput("message", message);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+const octokit = new Octokit({
+    userAgent: 'Punk Creator',
+    timeZone: 'Asia/Jakarta'
+})
+
+async function actionRun() {
+    try {
+        const owner = github.context.payload.repository.owner.name
+        const repo = github.context.payload.repository.name
+        const issue_number = github.context.payload.issue.number
+
+        const issue = await octokit.rest.issues.get({
+            owner,
+            repo,
+            issue_number,
+        });
+
+        console.log(`Hello Punk!`);
+        core.setOutput("message", issue)
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+actionRun()
