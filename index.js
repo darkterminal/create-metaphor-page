b28c9789391c541fd21151e2e0e8c7a5313db7e3
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-02-24 03:23:31 +0000

    chore: adding github access token input

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const { Octokit } = require("@octokit/rest");
 
 async function actionRun() {
     try {
+        const githubToken = core.getInput('github-token', { required: true });
+        const context = github.context;
+
         const octokit = new Octokit({
+            auth: githubToken,
             userAgent: 'Punk Creator',
             timeZone: 'Asia/Jakarta'
         })
         
-        const owner = github.context.payload.repository.owner.name
-        const repo = github.context.payload.repository.name
-        const issue_number = github.context.payload.issue.number
+        const owner = context.payload.repository.owner
+        const repo = context.payload.repository.name
+        const issue_number = context.payload.issue.number
 
         const issue = await octokit.rest.issues.get({
             owner,
